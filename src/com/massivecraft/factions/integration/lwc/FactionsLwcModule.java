Nrc0611/Factions-2.11

Split up EngineMain

src/com/massivecraft/factions/integration/lwc/FactionsLwcModule.java
@@ -1,16 +1,12 @@
 package com.massivecraft.factions.integration.lwc;
 
-import org.bukkit.Location;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
-
 import com.griefcraft.lwc.LWC;
 import com.griefcraft.model.Protection;
 import com.griefcraft.scripting.JavaModule;
 import com.griefcraft.scripting.event.LWCProtectionInteractEvent;
 import com.griefcraft.scripting.event.LWCProtectionRegisterEvent;
 import com.massivecraft.factions.Factions;
-import com.massivecraft.factions.engine.EngineMain;
+import com.massivecraft.factions.engine.EnginePerm;
 import com.massivecraft.factions.entity.MConf;
 import com.massivecraft.factions.entity.MPlayer;
 import com.massivecraft.massivecore.SoundEffect;
@@ -18,6 +14,9 @@ import com.massivecraft.massivecore.ps.PS;
 import com.massivecraft.massivecore.util.IdUtil;
 import com.massivecraft.massivecore.util.SmokeUtil;
 import com.massivecraft.massivecore.util.Txt;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 
 @SuppressWarnings("unused")
 public class FactionsLwcModule extends JavaModule
@@ -53,7 +52,7 @@ public class FactionsLwcModule extends JavaModule
 		
 		// ... and the player don't have build rights here ...
 		// NOTE: We verbosely check the build rights so that a proper info message is sent 
-		if (EngineMain.canPlayerBuildAt(event.getPlayer(), PS.valueOf(event.getBlock()), true)) return;
+		if (EnginePerm.canPlayerBuildAt(event.getPlayer(), PS.valueOf(event.getBlock()), true)) return;
 		
 		// ... then cancel the event.
 		event.setCancelled(true);
@@ -78,7 +77,7 @@ public class FactionsLwcModule extends JavaModule
 		// ... and if the protection owner no longer has build rights for the area ...
 		// NOTE: We silently check the build rights for the protection owner.
 		// NOTE: The protection owner may even be offline at the moment.
-		if (EngineMain.canPlayerBuildAt(mowner, ps, false)) return;
+		if (EnginePerm.canPlayerBuildAt(mowner, ps, false)) return;
 		
 		// ... remove the protection ...
 		protection.remove();

src/com/massivecraft/factions/integration/spigot/EngineSpigot.java
@@ -1,7 +1,13 @@
 package com.massivecraft.factions.integration.spigot;
 
-import java.util.List;
-
+import com.massivecraft.factions.engine.EnginePerm;
+import com.massivecraft.factions.entity.BoardColl;
+import com.massivecraft.factions.entity.Faction;
+import com.massivecraft.factions.entity.MConf;
+import com.massivecraft.factions.entity.MPerm;
+import com.massivecraft.massivecore.Engine;
+import com.massivecraft.massivecore.ps.PS;
+import com.massivecraft.massivecore.util.MUtil;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
@@ -11,14 +17,8 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.BlockPistonExtendEvent;
 import org.bukkit.event.block.BlockPistonRetractEvent;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
-import com.massivecraft.factions.engine.EngineMain;
-import com.massivecraft.factions.entity.BoardColl;
-import com.massivecraft.factions.entity.Faction;
-import com.massivecraft.factions.entity.MConf;
-import com.massivecraft.factions.entity.MPerm;
-import com.massivecraft.massivecore.Engine;
-import com.massivecraft.massivecore.ps.PS;
-import com.massivecraft.massivecore.util.MUtil;
+
+import java.util.List;
 
 
 public class EngineSpigot extends Engine
@@ -54,7 +54,7 @@ public class EngineSpigot extends Engine
 		if (entity.getType() != EntityType.ARMOR_STAND) return;
 		
 		// If we can't use ...
-		if (EngineMain.canPlayerUseEntity(player, entity, verboose)) return;
+		if (EnginePerm.canPlayerUseEntity(player, entity, verboose)) return;
 		
 		// ... block use.
 		event.setCancelled(true);

Hillimy/Factions

src/com/massivecraft/factions/integration/lwc/FactionsLwcModule.java
@@ -1,16 +1,12 @@
 package com.massivecraft.factions.integration.lwc;
 
-import org.bukkit.Location;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
-
 import com.griefcraft.lwc.LWC;
 import com.griefcraft.model.Protection;
 import com.griefcraft.scripting.JavaModule;
 import com.griefcraft.scripting.event.LWCProtectionInteractEvent;
 import com.griefcraft.scripting.event.LWCProtectionRegisterEvent;
 import com.massivecraft.factions.Factions;
-import com.massivecraft.factions.engine.EngineMain;
+import com.massivecraft.factions.engine.EnginePermBuild;
 import com.massivecraft.factions.entity.MConf;
 import com.massivecraft.factions.entity.MPlayer;
 import com.massivecraft.massivecore.SoundEffect;
@@ -18,6 +14,9 @@ import com.massivecraft.massivecore.ps.PS;
 import com.massivecraft.massivecore.util.IdUtil;
 import com.massivecraft.massivecore.util.SmokeUtil;
 import com.massivecraft.massivecore.util.Txt;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 
 @SuppressWarnings("unused")
 public class FactionsLwcModule extends JavaModule
@@ -53,7 +52,7 @@ public class FactionsLwcModule extends JavaModule
 		
 		// ... and the player don't have build rights here ...
 		// NOTE: We verbosely check the build rights so that a proper info message is sent 
-		if (EngineMain.canPlayerBuildAt(event.getPlayer(), PS.valueOf(event.getBlock()), true)) return;
+		if (EnginePermBuild.canPlayerBuildAt(event.getPlayer(), PS.valueOf(event.getBlock()), true)) return;
 		
 		// ... then cancel the event.
 		event.setCancelled(true);
@@ -78,7 +77,7 @@ public class FactionsLwcModule extends JavaModule
 		// ... and if the protection owner no longer has build rights for the area ...
 		// NOTE: We silently check the build rights for the protection owner.
 		// NOTE: The protection owner may even be offline at the moment.
-		if (EngineMain.canPlayerBuildAt(mowner, ps, false)) return;
+		if (EnginePermBuild.canPlayerBuildAt(mowner, ps, false)) return;
 		
 		// ... remove the protection ...
 		protection.remove();

src/com/massivecraft/factions/integration/spigot/EngineSpigot.java
@@ -1,7 +1,13 @@
 package com.massivecraft.factions.integration.spigot;
 
-import java.util.List;
-
+import com.massivecraft.factions.engine.EnginePermBuild;
+import com.massivecraft.factions.entity.BoardColl;
+import com.massivecraft.factions.entity.Faction;
+import com.massivecraft.factions.entity.MConf;
+import com.massivecraft.factions.entity.MPerm;
+import com.massivecraft.massivecore.Engine;
+import com.massivecraft.massivecore.ps.PS;
+import com.massivecraft.massivecore.util.MUtil;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
@@ -11,14 +17,8 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.BlockPistonExtendEvent;
 import org.bukkit.event.block.BlockPistonRetractEvent;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
-import com.massivecraft.factions.engine.EngineMain;
-import com.massivecraft.factions.entity.BoardColl;
-import com.massivecraft.factions.entity.Faction;
-import com.massivecraft.factions.entity.MConf;
-import com.massivecraft.factions.entity.MPerm;
-import com.massivecraft.massivecore.Engine;
-import com.massivecraft.massivecore.ps.PS;
-import com.massivecraft.massivecore.util.MUtil;
+
+import java.util.List;
 
 
 public class EngineSpigot extends Engine
@@ -54,7 +54,7 @@ public class EngineSpigot extends Engine
 		if (entity.getType() != EntityType.ARMOR_STAND) return;
 		
 		// If we can't use ...
-		if (EngineMain.canPlayerUseEntity(player, entity, verboose)) return;
+		if (EnginePermBuild.canPlayerUseEntity(player, entity, verboose)) return;
 		
 		// ... block use.
 		event.setCancelled(true);